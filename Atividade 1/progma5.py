429c4d92aedb97219e59f197cdfbd396bbd4cda2
andersonribeiro153-pixel <anderson_ribeiro153@estudante.sesisenai.org.br>
2026-03-20 20:19:30 +0000

    update

--- Atividade 1/progma5.py
+++ Atividade 1/progma5.py
@@ -1,3 +1,5 @@
 largura = float(input("digite a altura do terreno em metros"))
 largura = float(input("digite a altura do terreno em metros"))
 
+area = largura * altura
+print("are do meu terreno é:")
